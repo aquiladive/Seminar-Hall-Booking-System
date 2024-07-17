43e13e1bc759251fd4e0a19deda036cb7a96a4b7
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2024-07-17 08:49:42 +0530

    Update models.py
    Removed an unused model.

--- sbhs_app/models.py
+++ sbhs_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+#model for the HOD login
 class user(models.Model):
     username = models.CharField(max_length=60)
     email = models.EmailField()
@@ -8,6 +9,7 @@ class user(models.Model):
     def __str__(self):
         return self.username
 
+#model for the placement cell members
 class adminUser(models.Model):
     username = models.CharField(max_length=60)
     email = models.EmailField()
@@ -17,30 +19,6 @@ class adminUser(models.Model):
         return self.username
 
 
-#events that admin / placement cell approves to be added to calendar
-class approvedEvent(models.Model):
-    hall_choices = (
-        ("Ramegowda", "Ramegowda Seminar Hall"),
-        ("Training", "Training Hall"),
-    )
-
-    time_choices = (
-        ("9AM-1PM", "9 AM - 1 PM"),
-        ("1PM-5PM", "1 PM - 5 PM"),
-    )
-
-    name = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.CharField(max_length=10, choices=time_choices, default=time_choices[0])
-    description = models.CharField(max_length=300)
-    hall = models.CharField(max_length=10, choices=hall_choices, default=hall_choices[0])
-    dept = models.CharField(max_length=10)
-
-    def __unicode__(self):
-        return self.name
-    
-
-#events that the dept HOD is trying to book
 class event(models.Model):
     hall_choices = (
         ("Ramegowda", "Ramegowda Seminar Hall"),
